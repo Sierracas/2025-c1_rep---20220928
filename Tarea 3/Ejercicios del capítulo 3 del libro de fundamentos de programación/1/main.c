5e013497e4a38d824dbf5f9f25780e0c17db8244
Sierracas <144967395+Sierracas@users.noreply.github.com>
2025-02-07 05:13:49 -0400

    Add files via upload

---
+++ Tarea 3/Ejercicios del capítulo 3 del libro de fundamentos de programación/1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void main (void)
+{
+    int I;
+    float SAL, NOM;
+    NOM = 0;
+    for (I=1; I<=15; I++)
+    {
+        printf("\Ingrese el salario del profesor%d:\t", I);
+        scanf("%f", &SAL);
+        NOM = NOM + SAL;
+    }
+    printf("\nEl total de la nómina es: %.2f", NOM);
+}
